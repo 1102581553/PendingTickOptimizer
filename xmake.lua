26e75b9757ba17271d0d7a59947336a317e6f7e8
1102581553 <pheyeji@gmail.com>
2026-02-24 01:48:07 +0000

    Update xmake.lua

--- xmake.lua
+++ xmake.lua
@@ -1,41 +1,35 @@
-add_rules("mode.debug", "mode.release")
+set_project("PendingTickOptimizer")
+set_version("1.0.0")
 
 add_repositories("levimc-repo https://github.com/LiteLDev/xmake-repo.git")
-
-option("target_type")
-    set_default("server")
-    set_showmenu(true)
-    set_values("server", "client")
-option_end()
-
--- add_requires("levilamina x.x.x") for a specific version
--- add_requires("levilamina develop") to use develop version
--- please note that you should add bdslibrary yourself if using dev version
-add_requires("levilamina", {configs = {target_type = get_config("target_type")}})
-
+add_requires("levilamina 1.9.5", {configs = {target_type = "server"}})
 add_requires("levibuildscript")
 
 if not has_config("vs_runtime") then
     set_runtimes("MD")
 end
 
-target("my-mod") -- Change this to your mod name.
+if is_plat("windows") then
+    add_cxflags("/EHa", "/utf-8", "/W4", "/w44265", "/w44289", "/w44296", "/w45263", "/w44738", "/w45204")
+else
+    add_cxflags("-O2", "-march=native", "-flto=auto")
+end
+
+add_defines("NOMINMAX", "UNICODE")
+set_languages("c++20")
+set_optimize("fast")
+set_symbols("none")
+set_exceptions("none")
+add_includedirs("src")
+
+target("PendingTickOptimizer")
     add_rules("@levibuildscript/linkrule")
     add_rules("@levibuildscript/modpacker")
-    add_cxflags( "/EHa", "/utf-8", "/W4", "/w44265", "/w44289", "/w44296", "/w45263", "/w44738", "/w45204")
-    add_defines("NOMINMAX", "UNICODE")
-    add_packages("levilamina")
-    set_exceptions("none") -- To avoid conflicts with /EHa.
     set_kind("shared")
     set_languages("c++20")
-    set_symbols("debug")
-    add_headerfiles("src/**.h")
-    add_files("src/**.cpp")
-    add_includedirs("src")
-    if is_config("target_type", "server") then
-    --  add_includedirs("src-server")
-    --  add_files("src-server/**.cpp")
-    else
-    --  add_includedirs("src-client")
-    --  add_files("src-client/**.cpp")
-    end
+    add_headerfiles("src/*.h")
+    add_files("src/*.cpp")
+    add_packages("levilamina")
+    add_syslinks("shlwapi", "advapi32")
+    set_targetdir("bin")
+    set_runtimes("MD")
