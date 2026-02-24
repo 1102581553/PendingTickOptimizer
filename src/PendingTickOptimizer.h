b352c80a9cc0771dd35d080b3bacc9e9d57467dc
1102581553 <pheyeji@gmail.com>
2026-02-24 01:47:40 +0000

    Create PendingTickOptimizer.h

---
+++ src/PendingTickOptimizer.h
@@ -0,0 +1,41 @@
+#pragma once
+#include <ll/api/Config.h>
+#include <ll/api/io/Logger.h>
+#include <ll/api/mod/NativeMod.h>
+#include <memory>
+#include <atomic>
+
+namespace pending_tick_optimizer {
+
+struct Config {
+    int  version          = 1;
+    bool enabled          = true;
+    bool debug            = false;
+    int  statsIntervalSec = 5;
+
+    bool budgetEnabled = true;
+    int  budgetPerTick = 100; // 每 tick 最多处理 N 个计划刻
+};
+
+Config&         getConfig();
+bool            loadConfig();
+bool            saveConfig();
+ll::io::Logger& logger();
+
+class PluginImpl {
+public:
+    static PluginImpl& getInstance();
+
+    PluginImpl() : mSelf(*ll::mod::NativeMod::current()) {}
+
+    [[nodiscard]] ll::mod::NativeMod& getSelf() const { return mSelf; }
+
+    bool load();
+    bool enable();
+    bool disable();
+
+private:
+    ll::mod::NativeMod& mSelf;
+};
+
+} // namespace pending_tick_optimizer
